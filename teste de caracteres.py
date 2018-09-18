1ff0fb226aae2070e1020a20827552fc81f4d457
Alexandre <alexandre.abramson@hotmail.com>
2018-09-18 00:57:59 -0300

    testes em python

--- teste de caracteres.py
+++ teste de caracteres.py
@@ -1,11 +1,16 @@
-a = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 4, 5, 6, 5555, 4324, 1]
+a = []
 n = 0
-
-if len(a) > 3:
-    while n < len(a)-3:
+while 1:
+    try:
+        insert = float(input('Digite um número para colocar na pilha: '))
+        a.append(insert)
+    except:
+        break
+if len(a) >= 3:
+    while n <= len(a) - 3:
         print(a[n], end=', ')
         n += 1
-    print('%.i e %.i' % (a[len(a)-2], a[len(a)-1]))
+    print('%f e %f' % (a[len(a)-2], a[len(a)-1]))
 elif len(a) == 2:
     print(a[0], 'e', a[1])
 elif len(a) == 1:
