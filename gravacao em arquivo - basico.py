17aa0fea8b6a4d6b4cb8e32d58680e90b1765ff6
Alexandre Abramson <alexandre.abramson@hotmail.com>
2018-11-19 02:06:06 -0200

    teste básico de gravação de arquivo em python.

--- gravacao em arquivo - basico.py
+++ gravacao em arquivo - basico.py
@@ -1,11 +1,28 @@
 ### teste de gravação básica
+tamanho=[]
+arq=open ('carros.txt','r')
+print('********** Dados já imputados **********')
+print()
+print(arq.read())
+with open('carros.txt') as arq:
+    for i in arq:
+        tamanho.append(i)
+    print('Total de linhas: %i' % len(tamanho))
+    print()
 
-arq = open ('c:/temp/carros.txt','r')
-lista2=[]
-lista=arq.readlines()
-for i in lista:
-    lista2.append(i)
+while True:
+    novos=input('Por favor, insira outros carros na lista: ')
+    if not novos:break
+    with open('carros.txt','a+') as arq:
+        arq.writelines(novos)
+        arq.writelines('\n')
 
-print (lista2[::-1])
-
+lista=[]
+arq=open('carros.txt')
+for i in arq:
+    lista.append(i)
+print(lista)
 arq.close()
+
+#continuarei daqui: quero numerar a lista obtida da variável arq obtida da leitura do arquivo carros.txt e, após isso,
+#poderei excluir algum item baseado na numeração desse índice criado por um dicionário.
