a8ec290e638dff9f5dceb07b81b4a182f9aaa6c2
Alexandre <alexandre.abramson@hotmail.com>
2019-01-02 00:18:50 -0200

    teste básico de gravação de arquivo em python.

--- Cálculo de largura de banda.py
+++ Cálculo de largura de banda.py
@@ -1,17 +1,41 @@
 ### Programa para cálculo de tempo de download em relação a velocidade
 
-tamanho=float(input('Tamanho do arquivo (em MB): '))
-velocidade= float(input('Velocidade do link (em Mbps): '))
+while True:
+    tamanho_opcao = str(input('Cálculo do arquivo em MB ou mb (selecione M ou m)?: '))
+    if 'M' not in tamanho_opcao and 'm' not in tamanho_opcao:
+        print('Por favor, escolher entre Megabytes (M) ou megabits (m)')
+    else:
+        break
 
-#### Cálculo
-tamanho_mbps=tamanho*8
+tamanho = float(input('Tamanho do arquivo: '))
+velocidade = float(input('Velocidade do link (em Mbps): '))
+tamanho_real = 0
 
-tempo=tamanho_mbps/velocidade
+### Cálculo
 
+if tamanho_opcao == 'M':
+    tamanho_real = tamanho*8
+else:
+    tamanho_real = tamanho
 
+tempo = tamanho_real/velocidade
 
-segundos=tempo%60
-minutos=tempo//60
+segundos = tempo%60
+minutos = tempo//60
+minutos_corrigidos = minutos%60
+horas = minutos//60
+horas_corrigidas = horas%60
+dias = horas//24
+dias_corrigidos = dias%365
+anos = dias//365
 
-print('Tempo total de download: %.0f minutos e %.0f segundos.' % (minutos,segundos))
-
+if anos >= 1:
+    print('Tempo total de download: %.0f anos, %.0f dias, %.0f horas, %0.f minutos e %0.f segundos' % (anos, dias_corrigidos, horas_corrigidas, minutos_corrigidos, segundos))
+elif dias > 0:
+    print('Tempo total de download: %.0f dias, %.0f horas, %0.f minutos e %0.f segundos' % (dias, horas_corrigidas, minutos_corrigidos, segundos))
+elif horas > 0 and dias ==0:
+    print('Tempo total de download: %.0f horas, %0.f minutos e %0.f segundos' % (horas_corrigidas, minutos_corrigidos, segundos))
+elif minutos > 0 and horas == 0:
+    print('Tempo total de download: %.0f minutos e %.0f segundos.' % (minutos, segundos))
+elif (segundos > 0) and (segundos < 60) and minutos == 0 and horas == 0 and dias == 0:
+    print('Tempo total de download: %.0f segundos.' % (segundos))
